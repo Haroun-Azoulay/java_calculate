71dc437f71194b9fe747376e1856976e16e601f5
Haroun-Azoulay <ha.azoulay93@gmail.com>
2024-12-05 16:21:35 +0100

    [➕] Add new function to calculate number

--- src/java_calculate/Main.java
+++ src/java_calculate/Main.java
@@ -1,4 +1,5 @@
 package java_calculate;
+
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,8 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
 
         String userName = "";
-        
+        String calculateSymbol = "";
+
         while (true) {
             System.out.println("Enter your username:");
             userName = myObj.nextLine();
@@ -20,7 +22,56 @@ public class Main {
         }
 
         System.out.println("Welcome " + userName + "!");
+
+        System.out.print("Can you enter first number: ");
+        int a = myObj.nextInt();
+
+        System.out.print("Can you enter second number: ");
+        int b = myObj.nextInt();
+
+        myObj.nextLine();
         
+
+        System.out.println("Enter symbol to multiply (*), divide (/), or addition (+):");
+
+        while (true) {
+            calculateSymbol = myObj.nextLine().trim();
+
+            if (!calculateSymbol.isEmpty() &&
+                (calculateSymbol.equals("*") || calculateSymbol.equals("/") || calculateSymbol.equals("+"))) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter *, /, or +:");
+            }
+        }
+
+
+        if (calculateSymbol.equals("/") && b == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+        } else {
+            int result = myCalcule(a, b, calculateSymbol);
+            System.out.println("The result is: " + result);
+        }
+
         myObj.close();
     }
+
+    static int myCalcule(int a, int b, String calculateSymbol) {
+        int responseTotal = 0;
+        switch (calculateSymbol) {
+            case "*":
+                responseTotal = (a * b);
+                break;
+            case "/":
+                responseTotal = (a / b);
+                break;
+            case "+":
+                responseTotal = (a + b);
+                break;
+            default:
+                System.out.println("Invalid operation.");
+        }
+        return responseTotal;
+    }
 }
+
